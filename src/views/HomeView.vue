80a391622d694500750895f26ada688e866520aa
kat055 <gacusankatherine0514@gmail.com>
2022-03-09 21:16:26 -0800

    hello

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,23 @@
 
 <template>
 <body>
-  <h1>Cleo</h1>
+  <div class="navbar">
+    <img class="logo" src="https://dcassetcdn.com/design_img/1247936/127951/127951_6547961_1247936_41c10938_image.jpg">
+    <a href="home">Home</a>
+    <a href="features">Features</a>
+    <a href="workshop">Workshop</a>
+  </div>
 </body>
 </template>
+
+<style>
+  .logo {
+    height: 50px;
+    width:90px;
+  }
+
+  .navbar a {
+    color: white;
+    display: inline;
+  }
+</style>
